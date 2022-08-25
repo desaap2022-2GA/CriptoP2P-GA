27ee2d501b578fc2c9c532f11fd0597f399f8082
Cristian Gonzalez <cristian.gonzalez.unq@gmail.com>
2022-08-24 22:20:54 -0300

    create repository

---
+++ src/test/java/ar/edu/unq/desapp/GrupoA022022/backenddesappapi/BackendDesappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.GrupoA022022.backenddesappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDesappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
