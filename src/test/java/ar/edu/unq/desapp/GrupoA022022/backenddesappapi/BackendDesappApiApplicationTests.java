52e30634ffbc0b9ca95a313a9758105ecf9b1393
Cristian Gonzalez <cristian.gonzalez.unq@gmail.com>
2022-09-03 11:07:42 -0300

    gradle test dependencies

--- src/test/java/ar/edu/unq/desapp/GrupoA022022/backenddesappapi/BackendDesappApiApplicationTests.java
+++ src/test/java/ar/edu/unq/desapp/GrupoA022022/backenddesappapi/BackendDesappApiApplicationTests.java
@@ -4,9 +4,9 @@ import ar.edu.unq.desapp.GrupoA022022.backenddesappapi.Model.User;
 import org.springframework.boot.test.context.SpringBootTest;
 //import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
+//import static org.testng.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,16 +23,19 @@ class BackendDesappApiApplicationTests {
 		User user = new User();
 		String name = "Graciela";
 		user.setName(name);
+		System.out.println("en elNombreDeUnUsuarioEsCorrecto");
 
 		assertEquals(user.getName(), "Graciela");
 	}
 
 	@Test
-	void elNombreDeUnUsuarioNoCumpleLasCondicionesLanzaUnaException(){
+	void elNombreDeUnUsuarioNoCumpleLasCondicionesLanzaUnaExcepcion(){
 		User user = new User();
 		String name = "";
 		user.setName(name);
+		System.out.println("en elNombreDeUnUsuarioNoCumpleLasCondicionesLanzaUnaExcepcion");
 
+		assertEquals(1, 1);
 
 	}
 
