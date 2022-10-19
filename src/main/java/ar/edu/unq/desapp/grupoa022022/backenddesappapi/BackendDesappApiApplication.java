043844936a0efa23aaa100756343e15c77280c1f
Cristian Gonzalez <cristian.gonzalez.unq@gmail.com>
2022-10-19 19:55:47 -0300

    codeSmellsFix

--- src/main/java/ar/edu/unq/desapp/grupoa022022/backenddesappapi/BackendDesappApiApplication.java
+++ src/main/java/ar/edu/unq/desapp/grupoa022022/backenddesappapi/BackendDesappApiApplication.java
@@ -1,8 +1,5 @@
 package ar.edu.unq.desapp.grupoa022022.backenddesappapi;
 
-
-import ar.edu.unq.desapp.grupoa022022.backenddesappapi.persistence.IUserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
