0631bfc7e8596cfb6055fbd2d7adbfb0e3470361
Cristian Gonzalez <cristian.gonzalez.unq@gmail.com>
2022-10-25 13:17:35 -0300

    cleanImports

--- src/test/java/ar/edu/unq/desapp/grupoa022022/backenddesappapi/webservice/UserHttpRequestTest.java
+++ src/test/java/ar/edu/unq/desapp/grupoa022022/backenddesappapi/webservice/UserHttpRequestTest.java
@@ -1,13 +1,11 @@
 package ar.edu.unq.desapp.grupoa022022.backenddesappapi.webservice;
 
 import ar.edu.unq.desapp.grupoa022022.backenddesappapi.dto.*;
-import ar.edu.unq.desapp.grupoa022022.backenddesappapi.utils.IntentionType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
