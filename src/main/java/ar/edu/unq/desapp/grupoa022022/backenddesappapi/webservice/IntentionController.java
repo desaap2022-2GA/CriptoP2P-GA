c4543d597af785355fc60b1501f1cea8dc31734b
gradaglio <gradaglio@gmail.com>
2022-10-26 15:26:36 -0300

    Add swagger documentation

--- src/main/java/ar/edu/unq/desapp/grupoa022022/backenddesappapi/webservice/IntentionController.java
+++ src/main/java/ar/edu/unq/desapp/grupoa022022/backenddesappapi/webservice/IntentionController.java
@@ -5,6 +5,7 @@ import ar.edu.unq.desapp.grupoa022022.backenddesappapi.model.Intention;
 import ar.edu.unq.desapp.grupoa022022.backenddesappapi.model.exceptions.PriceNotInAValidRange;
 import ar.edu.unq.desapp.grupoa022022.backenddesappapi.model.exceptions.ResourceNotFound;
 import ar.edu.unq.desapp.grupoa022022.backenddesappapi.service.interfaceservice.IIntentionService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,28 +19,29 @@ public class IntentionController {
     @Autowired
     private IIntentionService intentionService;
 
+    @Operation(summary = "Create an intention")
     @PostMapping
     public Intention createIntention(@RequestBody @Valid IntentionRegister intentionRegister) throws ResourceNotFound, PriceNotInAValidRange {
         return intentionService.create(intentionRegister);
     }
 
+    @Operation(summary = "Search an intention by id")
     @GetMapping(value = "/{id}")
     public Intention getIntentionById(@PathVariable("id") Integer id) throws ResourceNotFound {
         return intentionService.findById(id);
     }
 
+    @Operation(summary = "List all intentions")
     @GetMapping
     public List<Intention> listAllIntentions() {
         return intentionService.getAll();
     }
 
+    @Operation(summary = "List all active intentions")
     @GetMapping(value = "/intention/active")
     public List<Intention> listIntentionActive() {
         return intentionService.getIntentionActive();
     }
 
-    @GetMapping(value = "/intention/{id}")
-    public Intention getIntentionById(int id) throws ResourceNotFound {
-        return intentionService.findById(id);
-    }
+
 }
--- src/main/java/ar/edu/unq/desapp/grupoa022022/backenddesappapi/webservice/OperationController.java
+++ src/main/java/ar/edu/unq/desapp/grupoa022022/backenddesappapi/webservice/OperationController.java
@@ -20,22 +20,23 @@ public class OperationController {
 
     @Autowired
     private IOperationService operationService;
-
+    @io.swagger.v3.oas.annotations.Operation(summary = "Start an operation")
     @PostMapping
     public OperationView openOperation(@RequestBody OperationRegister operationRegister) throws ResourceNotFound, IntentionAlreadyTaken, PriceExceedVariationWithRespectIntentionTypeLimits {
         return operationService.open(operationRegister);
     }
-
+    @io.swagger.v3.oas.annotations.Operation(summary = "Modify an operation")
     @PutMapping(value = "/{id}")
     public void modifyOperation(@RequestBody OperationModify operationModify) throws ResourceNotFound, InvalidState {
         operationService.modify(operationModify);
     }
 
+    @io.swagger.v3.oas.annotations.Operation(summary = "Search an operation by id")
     @GetMapping(value = "/{id}")
     public Operation getOperationById(@PathVariable("id") Integer id) throws ResourceNotFound {
         return operationService.findById(id);
     }
-
+    @io.swagger.v3.oas.annotations.Operation(summary = "List all operations")
     @GetMapping
     public List<Operation> listAllOperations() {
         return operationService.getAll();
